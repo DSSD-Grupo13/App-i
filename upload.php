33a1514511c2da949fe21cbc57ed00324b830362
EMILIANA <emiliana.corbatta@hotmail.com>
2017-11-14 14:21:42 -0300

    sube archivos a dropbox

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 
-/*SUBIDA LOCAL 
+
 $ds= DIRECTORY_SEPARATOR;  //1
 $storeFolder= $_POST['nomUsuario'].$_POST['idIncidente'];
 if (!file_exists($storeFolder)){
@@ -16,17 +16,18 @@ if (!empty($_FILES)) {
 
     move_uploaded_file($tempFile,$targetFile); //6
 
-}*/
-$storeFolder= $_POST['nomUsuario'].$_POST['idIncidente'];
+}
+
 $ch = curl_init("https://content.dropboxapi.com/2/files/upload");
-      $fp = fopen(realpath($file), 'rb');
+      $fp = fopen(realpath($targetFile), 'rb');
       curl_setopt($ch, CURLOPT_PUT, true);
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
       curl_setopt($ch, CURLOPT_INFILE, $fp);
-      curl_setopt($ch, CURLOPT_INFILESIZE, filesize(realpath($file)));
+      curl_setopt($ch, CURLOPT_INFILESIZE, filesize(realpath($targetFile)));
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       $app = "/grupo13dssd/";
       $path = $app.$storeFolder;
+      echo $storeFolder;
       $dropbox_key = "YI0Ljfi3WFAAAAAAAAAAEmBs4wrpw14xH6Nkq3MxKswKAW1XJijhs2SuNTo1Tunn";
       $headers = array();
       $headers[] = "Authorization: Bearer " . $dropbox_key;
@@ -39,4 +40,4 @@ $ch = curl_init("https://content.dropboxapi.com/2/files/upload");
           return false;
       }
       curl_close ($ch);
-?>  
+?>
